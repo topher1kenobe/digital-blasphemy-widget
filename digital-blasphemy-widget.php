0629e10f3e48516140cc911821c74a7c117499e7
Topher <topher@x-team.com>
2014-07-13 19:35:22 -0400

    Change include to require

--- digital-blasphemy-widget.php
+++ digital-blasphemy-widget.php
@@ -9,23 +9,23 @@ Version: 1.0
 /**
  * include the file that gets one random freebie
  */
-include_once( 'includes/random_freebie.class.php' );
+require_once 'includes/random_freebie.class.php';
 
 /**
  * include the file that gets the latest freebie
  */
-include_once( 'includes/latest_freebie.class.php' );
-$latest =  new Digital_Blasphemy_Latest_Freebie;
+require_once 'includes/latest_freebie.class.php';
+$latest = new Digital_Blasphemy_Latest_Freebie;
 
 /**
  * include the file that has the widget class
  */
-include_once( 'includes/make_widget.class.php' );
+require_once 'includes/make_widget.class.php';
 
 
 // register Digital_Blasphemy_Widget widget
 function register_digital_blasphemy_widget() {
-    register_widget( 'Digital_Blasphemy_Widget' );
+	register_widget( 'Digital_Blasphemy_Widget' );
 }
 add_action( 'widgets_init', 'register_digital_blasphemy_widget' );
 
