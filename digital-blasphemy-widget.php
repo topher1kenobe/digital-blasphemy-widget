5236dc30405256de3f66eee19194c886431f2f34
Topher DeRosia <topher@x-team.com>
2014-07-13 20:28:38 -0400

    Provide full paths for require Make files have - instead of _

--- digital-blasphemy-widget.php
+++ digital-blasphemy-widget.php
@@ -9,18 +9,18 @@ Version: 1.0
 /**
  * include the file that gets one random freebie
  */
-require_once 'includes/random_freebie.class.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/random-freebie.class.php';
 
 /**
  * include the file that gets the latest freebie
  */
-require_once 'includes/latest_freebie.class.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/latest-freebie.class.php';
 $latest = new Digital_Blasphemy_Latest_Freebie;
 
 /**
  * include the file that has the widget class
  */
-require_once 'includes/make_widget.class.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/make-widget.class.php';
 
 
 // register Digital_Blasphemy_Widget widget
